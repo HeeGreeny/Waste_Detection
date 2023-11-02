897a3c7994b65b36cffffc8567f143a941afa8e0
HeeGreeny <101893759+HeeGreeny@users.noreply.github.com>
2023-11-02 17:10:15 +0900

    Update app.py

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ if source_radio == settings.IMAGE:
         else:
             if st.sidebar.button('Detect Objects'):
                 res = model.predict(uploaded_image,
-                                    conf=confidence
+                                    conf=confidence, device='cuda',
                                     )
                 boxes = res[0].boxes
                 res_plotted = res[0].plot()[:, :, ::-1]
