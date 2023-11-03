11e731ad520c0bb0107f609fc41d8ff581261127
HeeGreeny <101893759+HeeGreeny@users.noreply.github.com>
2023-11-03 14:37:52 +0900

    Update settings.py

--- settings.py
+++ settings.py
@@ -28,23 +28,23 @@ DEFAULT_DETECT_IMAGE = IMAGES_DIR / 'A2C1_detected.jpg'
 
 # Videos config
 VIDEO_DIR = ROOT / 'videos'
-VIDEO_1_PATH = VIDEO_DIR / 'h_density_fast.mp4'
-VIDEO_2_PATH = VIDEO_DIR / 'h_density_slow.mp4'
-VIDEO_3_PATH = VIDEO_DIR / 'low_density_fast.mp4'
-VIDEO_4_PATH = VIDEO_DIR / 'low_density_slow.mp4'
-VIDEO_5_PATH = VIDEO_DIR / 'm_density_fast.mp4'
-VIDEO_6_PATH = VIDEO_DIR / 'm_density_slow2.mp4'
-VIDEO_7_PATH = VIDEO_DIR / 'm_density_very_fast.mp4'
-VIDEO_8_PATH = VIDEO_DIR / 'm_density_very_slow.mp4'
+VIDEO_1_PATH = VIDEO_DIR / 'h_density_fast_1.mp4'
+VIDEO_2_PATH = VIDEO_DIR / 'h_density_slow_1.mp4'
+VIDEO_3_PATH = VIDEO_DIR / 'm_density_slow2.mp4'
+# VIDEO_4_PATH = VIDEO_DIR / 'low_density_slow.mp4'
+# VIDEO_5_PATH = VIDEO_DIR / 'm_density_fast.mp4'
+# VIDEO_6_PATH = VIDEO_DIR / 'm_density_slow2.mp4'
+# VIDEO_7_PATH = VIDEO_DIR / 'm_density_very_fast.mp4'
+# VIDEO_8_PATH = VIDEO_DIR / 'm_density_very_slow.mp4'
 VIDEOS_DICT = {
     'video_1': VIDEO_1_PATH,
     'video_2': VIDEO_2_PATH,
     'video_3': VIDEO_3_PATH,
-    'video_4': VIDEO_4_PATH,
-    'video_5': VIDEO_5_PATH,
-    'video_6': VIDEO_6_PATH,
-    'video_7': VIDEO_7_PATH,
-    'video_8': VIDEO_8_PATH,
+    # 'video_4': VIDEO_4_PATH,
+    # 'video_5': VIDEO_5_PATH,
+    # 'video_6': VIDEO_6_PATH,
+    # 'video_7': VIDEO_7_PATH,
+    # 'video_8': VIDEO_8_PATH,
 }
 # ML Model config
 MODEL_DIR = ROOT / 'weights'
